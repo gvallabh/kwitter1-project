e795aaf434a89ff0332048a29217d82ff45973ba
gvallabh <84183211+gvallabh@users.noreply.github.com>
2022-02-19 17:48:55 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -16,11 +16,32 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+user_name= localstorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome"+ user_name + "!";
+
+function addRoom(){
+  room_name=document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+    purpose:"add from user"
+  });
+  localStorage.setItem("room_name" , room_name);
+  window.location="kwitter_page.html";
+}
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+         console.log("room name -" + Room_names);
+         row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+         document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+     
+    function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("room_name", name );
+      window.location="kwitter_page.html";
+    }
+
+
